abbef19175c9308b5e19abbe9b1afbc027b22fd5
jiidapa <jidapa.p@kkumail.com>
2019-09-12 17:00:32 +0700

    init project

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../style/style.scss'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className='footer mt-auto py-3 bg-dark text-white'>
+                <div className='container'>Place sticky footer content here.</div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/pages/AboutMe.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AboutMe = () => (
+  <div className="content">
+    <p></p>
+  </div>
+);
+
+export default AboutMe;
